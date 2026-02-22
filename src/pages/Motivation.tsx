0f01f64122a3fc701c5fc01bcf8ea580c648e797
kulwinder <kkour8585@gmail.com>
2026-02-22 22:00:16 +0530

    changes in notification page

--- src/pages/Motivation.tsx
+++ src/pages/Motivation.tsx
@@ -30,7 +30,7 @@ type Badge = {
 const motivationalQuotes = [
   "Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchill",
   "The only way to do great work is to love what you do. - Steve Jobs",
-  "Believe you can and you're halfway there and do whateve. - Theodore Roosevelt",
+  "Believe you can and you're halfway there and . - Theodore Roosevelt",
   "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
   "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
   "It does not matter how slowly you go as long as you do not stop. - Confucius",
